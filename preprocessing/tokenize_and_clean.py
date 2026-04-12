5917ea9bb9851f254fd6d1d120f92cb401ffa6a2
subrajith05 <subrajith.r@gmail.com>
2026-04-12 22:32:27 +0530

    dependencies fix 3

--- preprocessing/tokenize_and_clean.py
+++ preprocessing/tokenize_and_clean.py
@@ -1,8 +1,18 @@
+import nltk
+
+nltk.download('punkt', quiet=True)
+nltk.download('stopwords', quiet=True)
+nltk.download('wordnet', quiet=True)
+nltk.download('averaged_perceptron_tagger', quiet=True)
+
 from nltk.tokenize import word_tokenize
 from nltk.corpus import stopwords
 from nltk.stem import WordNetLemmatizer
 from nltk import pos_tag
 from nltk.corpus import wordnet
+
+
+from nltk.corpus import stopwords
 
 stop_words = set(stopwords.words('english'))
 lemmatizer = WordNetLemmatizer()
